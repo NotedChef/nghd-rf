f5408eb78d692bcaf6d73e0acbc5013f82361eb3
Barry <notedchef@gmail.com>
2019-05-26 14:30:06 +1000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nghd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'reactiveformsdemo';
+}
